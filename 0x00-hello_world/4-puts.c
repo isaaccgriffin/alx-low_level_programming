d84a80b191722876f48c2e35d510d0d66cc85d0a
isaaccgriffin <isaaccgriffin@gmail.com>
2023-06-15 12:46:59 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main . A program that prints a line with puts function
+ * main . A program in c that prints a line using  puts function
  * Return: 0 (Success)
  */
 int main(void)

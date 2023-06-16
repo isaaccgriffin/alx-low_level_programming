68f589483f025f162cdca084475b810596890eb4
isaaccgriffin <isaaccgriffin@gmail.com>
2023-06-16 13:04:49 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,12 +10,27 @@
 int main(void)
 
 {
-	int p, q;
+	int i, j;
 
-	for (p = 0; p <= 98; p++)
+	for (i = 0; i <= 99; i++)
 	{
-
-	for (q = p + 1; q <= 99; q++)
-	{
-
-		gvjfvjkfkfk
+		for (j = 0; j <= 99; j++)
+		{
+			if (i < j && i != j)
+			{
+				putchar((i / 10) + '0');
+				putchar((i % 10) + '0');
+				putchar(' ');
+				putchar((j / 10) + '0');
+				putchar((j % 10) + '0');
+				if (i != 98 || j != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}

4b45de4ca46ebcbd45fb27582dcf5a979bc0c5fe
isaaccgriffin <isaaccgriffin@gmail.com>
2023-06-16 04:16:00 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Prints single digit numbers of base 10 starting from 0 without char
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+	int d;
+
+	for (d = '0'; d <= '9' d++)
+	putchar(d);
+	putchar('\n');
+
+	return (0);
+
+}
---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - A program that prints the lowercase alphabet in reverse
+ * Return: Always 0 (Success)
+ */
+
+
+int main(void)
+
+{
+
+	char low;
+
+	for (low = 'z'; low >= 'a'; low--)
+	putchar(low);
+	putchar('\n');
+
+	return (0);
+
+}

e9afa776ba50c2a68f5656ff166f7720c7e995b6
isaaccgriffin <isaaccgriffin@gmail.com>
2023-06-16 02:54:52 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - A program in c that prints lowercase alphabets
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+	char c:
+
+	for (c = 'a'; c <= 'z'; C++)
+	putchar(c);
+	putchar('\n');
+	return (0);
+}

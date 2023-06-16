9c4ed5de761d80efa8c6e34568207ae0326b2d95
isaaccgriffin <isaaccgriffin@gmail.com>
2023-06-16 06:00:21 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -15,10 +15,10 @@ int main(void)
 	e = 'e';
 	q = 'q';
 
-	for (low = 'a; low <= 'z'; low++)
+	for (low = 'a'; low <= 'z'; low++)
 	{
 	if (low != e && low != q)
-	purchar(low);
+	putchar(low);
 	}
 	putchar('\n');
 	return (0);

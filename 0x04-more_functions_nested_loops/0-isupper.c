acf45bb44f27aa741344d504ab6aa746080fa728
isaaccgriffin <isaaccgriffin@gmail.com>
2023-06-22 00:45:20 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper - Checks for uppercase character
+ * @x: Number to be checked
+ * Return: 1 for uppercase letter or 0 for any else
+ */
+
+int _isupper(int x)
+
+{
+	if (x >= 65 && x <= 90)
+	{
+	return (1);
+	}
+	return (0);
+}

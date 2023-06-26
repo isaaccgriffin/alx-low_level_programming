c1b269bdfcf139136de4017db8e14ec0ae21f33f
isaaccgriffin <isaaccgriffin@gmail.com>
2023-06-26 03:03:34 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,25 +1,26 @@
 #include "main.h"
 
 /**
-* puts2 - Prints every other character of a string
-* @str: String to be treated
-* Return: Void
-*/
+ * puts - It prints every other character of a string
+ * @str: String to be treated
+ * Return: Void
+ */
 
 void puts2(char *str)
 
 {
-int i;
-int j = 0;
+	int i;
+	int j = 0;
 
-while (str[j] != '\0')
-{
-j++;
-}
+	while (str[j] != '\0')
+	{
+	j++;
+	}
 
-for (i = 0; i < j; i += 2)
-{
--putchar(str[i]);
-}
-_putchar('\n');
+	for (i = 0; i < j; i += 2)
+	{
+	_putchar(str[i]);
+	}
+	_putchar('\n');
+
 }

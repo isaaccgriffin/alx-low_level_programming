b97034a8185ced09325dbc4c6a0bd8ab466fa7d4
isaaccgriffin <isaaccgriffin@gmail.com>
2023-08-01 01:31:16 -0700

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- * insert_nodeint_at_index - a function that inserts a new node at a given position
+ * insert_nodeint_at_index - a function that inserts new node at given position
  * @head: first node
  * @idx: index where the new node is summed
  * @n:  new node
